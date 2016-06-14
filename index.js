f93747a700874b4c5ff61595baabdf042f69de49
a-wenzel <wenzel.andre@gmail.com>
2016-06-14 10:15:40 +0200

    Commit

--- index.js
+++ index.js
@@ -75,6 +75,37 @@ controller.hears(['roman'], ['direct_message', 'direct_mention','in_channel'], f
     bot.reply(message, 'Ja <@' + message.user + '> du hast recht, Roman ist wirklich ein Homo!')
 })
 
+ontroller.hears(['uptime', 'identify yourself', 'who are you', 'what is your name'],
+    ['direct_message,direct_mention,mention'], function (bot, message) {
+
+        var hostname = os.hostname();
+        var uptime = formatUptime(process.uptime());
+
+        bot.reply(message,
+            ':robot_face: I am a bot named <@' + bot.identity.name +
+            '>. I have been running for ' + uptime + ' on ' + hostname + '.');
+
+    });
+
+function formatUptime(uptime) {
+    var unit = 'second';
+    if (uptime > 60) {
+        uptime = uptime / 60;
+        unit = 'minute';
+    }
+    if (uptime > 60) {
+        uptime = uptime / 60;
+        unit = 'hour';
+    }
+    if (uptime != 1) {
+        unit = unit + 's';
+    }
+
+    uptime = uptime + ' ' + unit;
+    return uptime;
+}
+
+/* Last */
 controller.hears('.*', ['direct_message', 'direct_mention'], function (bot, message) {
-    bot.reply(message, 'Sorry <@' + message.user + '>, I don\'t understand. \n')
+    bot.reply(message, '<@' + message.user + '> du Depp - dafür gibts noch keinen Befehl. \n')
 })
